71a7ae7efa6ad7350090278bfe17640de3778854
De Carvalho <wdecarvalho@excilys.com>
2018-04-26 13:08:44 +0200

    [STATE] : H2 desormais en memoire

--- src/test/java/com/excilys/dao/ComputerDaoTest.java
+++ src/test/java/com/excilys/dao/ComputerDaoTest.java
@@ -15,6 +15,7 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.h2.tools.RunScript;
 import org.junit.jupiter.api.BeforeAll;
@@ -60,7 +61,7 @@ public class ComputerDaoTest {
      * Verifie que la recherche d'un ordinateur existant fonctionne.
      */
     @Test
-    @DisplayName("Test find an existing computer")
+    @DisplayName("Should return the computer with ID 1L")
     public void findExistingComputerTest() {
         final Company company = new Company.Builder(1L).name("Apple Inc.").build();
         assertEquals(new Computer.Builder("MackBook Pro 15.4 inch").id(1L).introduced(null).discontinued(null)
@@ -72,7 +73,7 @@ public class ComputerDaoTest {
      * NoSuchElementException.
      */
     @Test
-    @DisplayName("Test find a computer that not exist")
+    @DisplayName("Should throw NoSuchElementException for id 111L (ID not existing)")
     public void findNotExistingComputerTest() {
         assertThrows(NoSuchElementException.class, () -> computerDao.find(111L).get());
     }
@@ -82,7 +83,7 @@ public class ComputerDaoTest {
      * NoSuchElementException.
      */
     @Test
-    @DisplayName("Test find a computer with invalid Long")
+    @DisplayName("Should throw NoSuchElementException for id -1L (ID not possible)")
     public void findComputerWithInvalidIDTest() {
         assertThrows(NoSuchElementException.class, () -> computerDao.find(-1L).get());
     }
@@ -91,7 +92,7 @@ public class ComputerDaoTest {
      * Verifie que findAll recupere tout les computers.
      */
     @Test
-    @DisplayName("Test to get all computers")
+    @DisplayName("Should return true when find all computer")
     public void findAllcomputerTest() {
         assertTrue(computerDao.findAll().size() >= 11);
     }
@@ -101,17 +102,17 @@ public class ComputerDaoTest {
      * (retour a la derniere).
      */
     @Test
-    @DisplayName("Test to get computers by page result")
+    @DisplayName("Should return the page requested by the user (1 and 2 are requested and found)")
     public void findcomputerByPageTest() {
         assertEquals(1, computerDao.findPerPage(1).getPageCourante());
-        assertEquals(1, computerDao.findPerPage(2).getPageCourante());
+        assertEquals(2, computerDao.findPerPage(2).getPageCourante());
     }
 
     /**
      * Verifie que la recuperation de page incorrecte affiche bien la premeire page.
      */
     @Test
-    @DisplayName("Test to get computers by impossible page result")
+    @DisplayName("Should return the page 1 when the pages requested are under 1")
     public void findComputerByPageNotPossibleTest() {
         assertEquals(1, computerDao.findPerPage(0).getPageCourante());
         assertEquals(1, computerDao.findPerPage(-1).getPageCourante());
@@ -125,7 +126,7 @@ public class ComputerDaoTest {
      * Verifie que la creation d'un computer sans companie fonctionne.
      */
     @Test
-    @DisplayName("Test create a valid computer without company")
+    @DisplayName("Should create a computer when is correctly filled without company")
     public void createValidComputerWithoutCompanyTest() {
         final Long id = computerDao.create(
                 new Computer.Builder("PC_NAME").introduced(LocalDate.now()).discontinued(LocalDate.now()).build());
@@ -137,12 +138,12 @@ public class ComputerDaoTest {
      * pas, met la companie a null.
      */
     @Test
-    @DisplayName("Test create a valid computer with a company that not exist")
+    @DisplayName("Should not create a computer when ID 111L for a company is used (111L not exist)")
     public void createValidComputerWithCompanyNotExistingTest() {
         final Company company = new Company.Builder(111L).build();
         final Long id = computerDao.create(new Computer.Builder("PC_COMPANY_NOT").introduced(LocalDate.now())
                 .discontinued(LocalDate.now()).company(company).build());
-        assertNull(computerDao.find(id).get().getCompany());
+        assertEquals(Optional.empty(), computerDao.find(id));
     }
 
     /**
@@ -150,7 +151,7 @@ public class ComputerDaoTest {
      * fonctionne.
      */
     @Test
-    @DisplayName("Test create a valid computer with a company that exist")
+    @DisplayName("Should create a computer when is correctly filled with a valid company")
     public void createValidComputerWithCompanyExistingTest() {
         final Company company = new Company.Builder(1L).build();
         final Long id = computerDao.create(new Computer.Builder("PC_COMPANY").introduced(LocalDate.now())
@@ -162,7 +163,7 @@ public class ComputerDaoTest {
      * Verifie que la création d'un computer "leger" fonctionne.
      */
     @Test
-    @DisplayName("Test create an empty computer")
+    @DisplayName("Should create a light computer (with minimum required)")
     public void createValidLightComputerTest() {
         final Long id = computerDao.create(new Computer.Builder(null).build());
         assertNotEquals(-1L, id);
@@ -175,10 +176,11 @@ public class ComputerDaoTest {
     /**
      * Verifie que la mise a jour d'un ordinateur qui n'est pas en BD deleche une
      * ComputerNeedIdToBeUpdateException.
-     * @throws ComputerNeedIdToBeUpdateException  Si l'ID n'est pas présent
+     * @throws ComputerNeedIdToBeUpdateException
+     *             Si l'ID n'est pas présent
      */
     @Test
-    @DisplayName("Test update computers not existing in BD")
+    @DisplayName("Should not update computer with no ID and throw ComputerNeedIdToBeUpdateException and should not update computer with ID 1111L (no present in database)")
     public void updateComputerTransientTest() throws ComputerNeedIdToBeUpdateException {
         final Computer computer = new Computer.Builder(null).build();
         assertThrows(ComputerNeedIdToBeUpdateException.class, () -> computerDao.update(computer));
@@ -192,7 +194,7 @@ public class ComputerDaoTest {
      *             Si le computer n'a pas d'ID
      */
     @Test
-    @DisplayName("Test update a computer existing in BD")
+    @DisplayName("Should update the computer 1L (existing in dtabase)")
     public void updateComputerExistingTest() throws ComputerNeedIdToBeUpdateException {
         final Computer computer = new Computer.Builder("rename").id(9L).introduced(LocalDate.now())
                 .discontinued(LocalDate.parse("2015-02-02", DateTimeFormatter.ISO_LOCAL_DATE))
@@ -212,7 +214,7 @@ public class ComputerDaoTest {
      * Test la suppresion d'un computer qui existe et d'un qui n'existe pas.
      */
     @Test
-    @DisplayName("Test delete computers that exist and not exist")
+    @DisplayName("Should delete the computer 12L (existing in database) and not delete the computer 111L (not existing in database)")
     public void suppresionComputerExisting() {
         assertFalse(computerDao.delete(111L));
         assertTrue(computerDao.delete(12L));
